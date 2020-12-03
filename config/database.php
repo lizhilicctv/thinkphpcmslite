b0be29a26e267fbca543464c53b9c4fc46f4666c
lizhili <lizhilimaster@163.com>
2020-12-03 09:15:01 +0800

    添加支付，升级框架

--- config/database.php
+++ config/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'thinkphpcmslite',
+    'database'        => '_lizhili_database',
     // 用户名
-    'username'        => 'thinkphpcmslite',
+    'username'        => '_lizhili_username',
     // 密码
-    'password'        => 'thinkphpcmslite',
+    'password'        => '_lizhili_password',
     // 端口
     'hostport'        => '',
     // 连接dsn
